abb28c606e7bb48f3b78836ff31d95c4dd1082f5
JimHartung620 <jimboslice1024@gmail.com>
2020-11-23 16:19:10 -0600

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using TheWall.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+
+namespace TheWall.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+
+        public HomeController(MyContext context)
+        {
+            _context = context;
+        }
+
+        
+
+    /* ---------------------------------------- Views ----------------------------------------------------------- */
+
+        
+
+    /* ---------------------------------------- Login/Reg ------------------------------------------------------- */
+
+        
+
+    /* ---------------------------------------- Form Submissions ------------------------------------------------ */
+
+        
+
+    /* ---------------------------------------- Actions --------------------------------------------------------- */
+
+        
+
+    }
+}
